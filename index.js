9024e7592980fbcc0018fe61b2b59b06898e4822
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-01-16 19:57:07 +0300

    add size constructors

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
 
-   
+    function Pizza(Size, pizzaCrust, pizzaToppings, PizzaNumber) {
+            this.pizzaSize = pizzaSize;
+            this.pizzaCrust = pizzaCrust;
+            this.pizzaToppings = pizzaTopping;
+            this.PizzaNumber = PizzaNumber;
+          }
+
+  //size
+  function Size(size, price) {
+    this.size = size;
+    this.price = price;
+  }
 
 });
